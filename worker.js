6e16da7628b6d423fdcca60f9594c2900c4a597c
krishpranav <krisna.pranav@gmail.com>
2021-04-23 16:29:19 +0530

    update status and log function

--- worker.js
+++ worker.js
@@ -9,3 +9,13 @@ var workerId
 var interval = 100000
     , count = 0
     , startTime = +new Date
+
+// status function
+function status(msg) {
+    this.postMessage( {cmd: "status", data:msg, id: workerId })
+}
+
+// log function
+function log(msg) {
+    this.postMessage( {cmd: "log", data: msg, id: workerId })
+}
